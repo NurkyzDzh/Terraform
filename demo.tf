84e3e646a7f2f75f92732af033995013e00107d1
NurkyzDzh <73071346+NurkyzDzh@users.noreply.github.com>
2021-04-22 11:08:05 -0500

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -71,20 +71,34 @@ resource "aws_security_group" "terraform_SG" {
   vpc_id      = aws_vpc.tf_vpc.id
     
   ingress {
-    for_each = ["80", "22", "443", "8080"]
-    content { 
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }  
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 }
+    ingress {
+    description = "HTTP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+    ingress {
+    description = "SSH"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    
   tags = {
     Name = "allow_web"
   }
